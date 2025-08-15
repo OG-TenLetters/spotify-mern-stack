158bce62f1805d9b484e86e53267c26b355d789a
OG-TenLetters <1tenletters0@gmail.com>
2025-08-15 16:47:30 -0700

    Why is this buggin

--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -2,13 +2,16 @@ import { axiosInstance } from "@/lib/axios";
 import type { Album, Song } from "@/types";
 import { create } from "zustand";
 
+
 interface MusicStore {
     songs: Song[];
     albums: Album[];
     isLoading: boolean;
     error: string | null;
+    currentAlbum: Album | null;
     
     fetchAlbums: () => Promise<void>; 
+    fetchAlbumById: (id: string) => Promise<void>;
 
 }
 
@@ -18,6 +21,7 @@ export const useMusicStore = create<MusicStore>((set) => ({
     songs: [],
     isLoading: false,
     error: null,
+    currentAlbum: null,
 
   fetchAlbums: async () => {
     set({
@@ -33,4 +37,16 @@ export const useMusicStore = create<MusicStore>((set) => ({
       set({ isLoading: false });
     }
   },
+
+  fetchAlbumById: async (id) => {
+    set({ isLoading: true, error: null})
+    try {
+      const response = await axiosInstance.get(`/albums/${id}`)
+      set({ currentAlbum: response.data })
+    } catch (error:any) {
+      set({ error: error.response.data.message})
+    } finally {
+      set({isLoading: false})
+    }
+  }
 }));
